2fe9c45df60049b2c9f12c6cb1647762d72da9b2
Levani Chargeishvili <chargeishvililevani@gmail.com>
2024-08-12 11:31:23 +0400

    vercel build error

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 
 
 
@@ -14,6 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 export { app };
